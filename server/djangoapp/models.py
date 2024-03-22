b871d8fc559a104937ba6cd05746237b70fab52b
IK <you@example.com>
2024-03-22 17:33:32 -0400

    lint compliance #2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -19,15 +19,6 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
@@ -43,10 +34,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-                                validators=[
-                                    MaxValueValidator(2023),
-                                    MinValueValidator(2015)
-                                ])
+                            validators=[
+                                MaxValueValidator(2023),
+                                MinValueValidator(2015)
+                            ])
 
     def __str__(self):
         return self.name  # Return the name as the string representation
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://ikarigbeosul-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://ikarigbeosul-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://ikarigbeosul-8000.theianext-1-labs-prod-misc-tools-us-east-0\
+    .proxy.cognitiveclass.ai',
+    'https://ikarigbeosul-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01\
+    .proxy.cognitiveclass.ai'
     ]
-CSRF_TRUSTED_ORIGINS = ['https://ikarigbeosul-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 'https://ikarigbeosul-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = [
+    'https://ikarigbeosul-8000.theianext-1-labs-prod-misc-tools-us-east-0\
+    .proxy.cognitiveclass.ai',
+    'https://ikarigbeosul-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01\
+    .proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -98,7 +104,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.\
+        UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -143,8 +150,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
